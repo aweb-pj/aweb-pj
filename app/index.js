f130a0016c323374a8b85bb91f1b8cfd98e74242
wjt <jtwang.me@gmail.com>
2017-03-26 22:04:08 +0800

    a small example from zhihu.com

--- app/index.js
+++ app/index.js
@@ -1,20 +1,56 @@
+'use strict'
+
 import * as THREE from 'three'
-let renderer = new THREE.WebGLRenderer()
-renderer.setSize(400, 300)
-document.getElementsByTagName('body')[0].appendChild(renderer.domElement)
+import _ from 'lodash'
 
-let scene = new THREE.Scene()
-let camera = new THREE.OrthographicCamera(-1, 1, 1.5, -1.5, 1, 10)
-camera.position.set(0, 0, 5)
-
+let scene, camera, renderer
+scene = new THREE.Scene()
+camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight)
+camera.position.set(3, 4, 5)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
 scene.add(camera)
 
-let cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe: true
-  }))
+let regionR = 3
+let cylinderMesh = new THREE.CylinderGeometry(regionR, regionR, 0.05, 1024)
+let material = new THREE.MeshNormalMaterial()
+let cylinder = new THREE.Mesh(cylinderMesh, material)
+cylinder.position.set(0, 0, 0)
+scene.add(cylinder)
 
-scene.add(cube)
+function pointsGenerator (amount) {
+  let pointsArr = []
+  pointsArr = _.map(_.range(0, amount), () => {
+    let r = Math.random() * regionR
+    let θ = (2 * Math.PI / 360) * (Math.random() * 360)
+    let polarPoint = {r, θ}
 
+    let x = r * Math.cos(θ)
+    let y = r * Math.sin(θ)
+    let cartesianPoint = {x, y}
+    return {polarPoint, cartesianPoint}
+  })
+
+  return pointsArr
+}
+
+function cubesGenerator (amount) {
+  let cubesArr = []
+
+  cubesArr = _.map(pointsGenerator(amount), ({polarPoint, cartesianPoint}) => {
+    let cubeMesh = new THREE.BoxGeometry(0.2, regionR - polarPoint.r, 0.2)
+    let cube = new THREE.Mesh(cubeMesh, material)
+    cube.position.set(cartesianPoint.x, (regionR - polarPoint.r) / 2 + 0.025, cartesianPoint.y)
+    return cube
+  })
+
+  return cubesArr
+}
+
+_.forEach(cubesGenerator(100), (cube) => {
+  scene.add(cube)
+})
+
+renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 renderer.render(scene, camera)
